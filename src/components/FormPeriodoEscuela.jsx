e9a17363f05e641bad326505ffd6d4bbc7361acc
mterrera29 <matiasterrera29@gmail.com>
2023-09-15 13:33:25 -0300

    final

--- src/components/FormPeriodoEscuela.jsx
+++ src/components/FormPeriodoEscuela.jsx
@@ -2,34 +2,16 @@
 
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-
-import { useState } from 'react';
+import { Card } from '@mui/material';
 import { dataItems } from '../../data';
 import DatePeriodoSelect from './DatePeriodoSelect';
 
 const FormPeriodoEscuela = ({register, escuela, dataLocal}) => {
-  const [expanded, setExpanded] = useState('panel1');
-
-  const handleChange = (panel) => (event, newExpanded) => {
-    setExpanded(newExpanded ? panel : false);
-  };
-
   return (
     <>
-      <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')} className="mb-3 mt-3 custom-accordion ">
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography><strong>Periodo de Calificación</strong></Typography>
-        </AccordionSummary>
-        <AccordionDetails>
+      <Card style={{padding:"20px", marginBottom:"10px"}}>
+          <Typography style={{textAlign:"center"}}><strong>Periodo de Calificación</strong></Typography>
           <Form.Group className="mb-3 "> 
             <Typography>Desde: </Typography>
               <Row className="align-items-center mt-1" style={{display:"flex", flexDirection:"row", flexWrap:"nowrap"}}>
@@ -50,8 +32,7 @@ const FormPeriodoEscuela = ({register, escuela, dataLocal}) => {
               }
             </Row>
           </Form.Group>
-        </AccordionDetails>
-      </Accordion>
+      </Card>
     </>
   )
 }
